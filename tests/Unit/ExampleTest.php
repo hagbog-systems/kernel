68b444937e8c447db2c7dd0dbf4106de986d0c54
jstoone <jstoone@drk.sh>
2019-06-12 00:21:19 +0200

    Initial Hag in the Bog of Systems.

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testBasicTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
